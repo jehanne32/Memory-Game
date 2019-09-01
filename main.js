c0376fcd06359171699faac839ceb6d0d1743eca
Michelle Jensen <mchlljnsn@yahoo.com>
2019-09-01 13:15:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+
+
+if (cardInPlay) {
+  cardInPlay.length === 2;
+  if cardsInPlay[0] === cardsInPlay[1]
+}
+
+alert("You found a match!");
+
+function flipCard(cardID) {
+  console.log("User flipped " + cards[cardID].rank);
+  console.log("User flipped " + cards[cardID].rank);
+}
+flipCard(0);
+flipCard(2);
+
+function checkForMatch(): {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+
+}
+
+checkForMatch();
+  cards = [
+    {
+      suit: "diamonds",
+      rank: "king",
+      cardImage: "images/king-of-diamonds.png"
+    },
+    {
+      suit: "hearts",
+      rank: "king",
+      cardImage: "images/king-of-hearts.png"
+    },
+    {
+      suit: "diamonds",
+      rank: "queen",
+      cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+      suit: "hearts",
+      rank: "queen",
+      cardImage: "images/queen-of-hearts.png"
+    }
+  ];
+
+  cardsInPlay = (cards[cardID]);
+
+  var cardOne = "queen";
+
+  cardsInPlay.push("rank")(cards[cardID]);
+  console.log(cardImage);
+  console.log(suit);
+
+  console.log("User flipped queen");
+
+  var cardTwo = "king";
+
+  cardsInPlay.push("rank)(cards[cardID]);
+
+  console.log("User flipped king");
+}
